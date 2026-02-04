f5968b842d53271b66062b1dc8b9e3843b3086ab
Горелик Егор <egorelik@softmedialab.com>
2026-02-05 01:32:00 +0500

    - Добавление цикла с перебором массива - Добавление цикла на поиск простых чисел

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const num = 266219;
 
 const initialValue = 1;
 let result = Array.from(String(num)).reduce(
-    (acc, currentValue) => acc * currentValue,
-    initialValue,
+  (acc, currentValue) => acc * currentValue,
+  initialValue,
 );
 // можно было сразу возвести в степень, но для наглядности сделал в 2 этапа
 
@@ -26,53 +26,53 @@ console.log(Number(sliceResult));
 // Задание про дни недели
 
 const weekDaysRu = [
-    "Понедельник",
-    "Вторник",
-    "Среда",
-    "Четверг",
-    "Пятница",
-    "Суббота",
-    "Воскресенье",
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+  "Воскресенье",
 ];
 const weekDaysEn = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
 ];
 
 let lang = "ru";
 
 if (lang === "ru") {
-    console.log(weekDaysRu);
+  console.log(weekDaysRu);
 } else if (lang === "en") {
-    console.log(weekDaysEn);
+  console.log(weekDaysEn);
 } else {
-    console.log("Язык не поддерживается");
+  console.log("Язык не поддерживается");
 }
 
 // через switch
 lang = "en";
 
 switch (lang) {
-    case "ru":
-        console.log(weekDaysRu);
-        break;
-    case "en":
-        console.log(weekDaysEn);
-        break;
-    default:
-        console.log("Язык не поддерживается");
+  case "ru":
+    console.log(weekDaysRu);
+    break;
+  case "en":
+    console.log(weekDaysEn);
+    break;
+  default:
+    console.log("Язык не поддерживается");
 }
 
 // через многомерный массив
 
 const obj = {
-    ru: weekDaysRu,
-    en: weekDaysEn,
+  ru: weekDaysRu,
+  en: weekDaysEn,
 };
 
 console.log(obj[lang] || "Язык не поддерживается");
@@ -82,11 +82,11 @@ console.log(obj[lang] || "Язык не поддерживается");
 const namePerson = "Артем";
 
 const role =
-    namePerson === "Артем"
-        ? "Директор"
-        : namePerson === "Александр"
-          ? "Преподаватель"
-          : "Студент";
+  namePerson === "Артем"
+    ? "Директор"
+    : namePerson === "Александр"
+      ? "Преподаватель"
+      : "Студент";
 
 console.log(role);
 
@@ -95,27 +95,43 @@ console.log(role);
 const longString = "Это длинная строка для проверки работы функции";
 
 const formatedString = function (str) {
-    let deleteSpaces = "";
+  let deleteSpaces = "";
 
-    if (typeof str === "string") {
-        deleteSpaces = str.trim();
-    } else {
-        return "Переданный аргумент не является строкой";
-    }
+  if (typeof str === "string") {
+    deleteSpaces = str.trim();
+  } else {
+    return "Переданный аргумент не является строкой";
+  }
 
-    return deleteSpaces.length > 30
-        ? `${deleteSpaces.slice(0, 30)}...`
-        : deleteSpaces;
+  return deleteSpaces.length > 30
+    ? `${deleteSpaces.slice(0, 30)}...`
+    : deleteSpaces;
 };
 
 console.log(formatedString(longString));
 
 // lesson 5
 
-const arr = ["423", "25", "993", "777", "83", "48", "99"];
+const arr = ["423", "25", "903", "777", "83", "48", "99"];
 
 for (let i = 0; i < arr.length; i++) {
-    if (Number(arr[i][0]) === 2 || Number(arr[i][0]) === 4) {
-        console.log(arr[i]);
+  if (arr[i][0] === "2" || arr[i][0] === "4") {
+    console.log(arr[i]);
+  }
+}
+
+for (let i = 2; i <= 100; i++) {
+  let isSimple = true;
+
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      isSimple = false;
+      break;
     }
+  }
+
+  if (isSimple) {
+    let resultText = `Делители этого числа: 1 и ${i}`;
+    console.log(i, resultText);
+  }
 }
